c75663442858b24f610ebf4bc1cbf247545fee46
minidkz <minidkz@gmail.com>
2021-10-18 12:35:30 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 password='password'
+allowusers='AllowUsers opc pingu'
 
 dnf upgrade -y
 hostnamectl set-hostname jump.nisseland.local
@@ -8,4 +9,4 @@ echo $password | passwd --stdin pingu
 usermod -aG wheel pingu
 sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
 sed -i 's/PermitRootLogin=no/g' /etc/ssh/sshd_config
-sed -i 's/Allowusers opc pingu/g' /etc/ssh/sshd_config
+sed -i '$allowusers' /etc/ssh/sshd_config
