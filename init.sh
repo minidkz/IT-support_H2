507e161207dc499b04fd514bcb163f714454aec8
minidkz <minidkz@gmail.com>
2021-10-18 11:24:34 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,3 +7,5 @@ useradd pingu
 echo $password | passwd --stdin pingu
 usermod -aG wheel pingu
 sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+sed -i 's/PermitRootLogin=no/g' /etc/ssh/sshd_config
+sed -i 's/Allowusers opc pingu/g' /etc/ssh/sshd_config
