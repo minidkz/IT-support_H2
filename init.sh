aee3b30c1e5f27cbc464d22daeafbb50befc20d2
minidkz <minidkz@gmail.com>
2021-10-19 08:26:15 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,4 +7,10 @@ echo password | passwd --stdin pingu
 usermod -aG wheel pingu
 sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
 sed -i 's/PermitRootLogin=no/g' /etc/ssh/sshd_config
-sed \i 'AllowUsers opc pingu' /etc/ssh/sshd_config
+echo AllowUsers opc pingu >> /etc/ssh/sshd_config
+firewall-cmd --perm --zone=internal --change-interface=enp1s0 
+firewall-cmd --perm --zone=internal --remove-service=mdns
+firewall-cmd --perm --zone=internal --samba-client
+firewall-cmd --perm --zone=internal --dhcpv6-client
+sleep 5s
+reboot now
