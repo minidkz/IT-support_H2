1980f33c83ec7aac107f6e352087389c53c107f4
minidkz <minidkz@gmail.com>
2021-10-19 08:40:14 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ sed -i 's/PermitRootLogin=no/g' /etc/ssh/sshd_config
 echo AllowUsers opc pingu >> /etc/ssh/sshd_config
 firewall-cmd --perm --zone=internal --change-interface=enp1s0 
 firewall-cmd --perm --zone=internal --remove-service=mdns
-firewall-cmd --perm --zone=internal --samba-client
-firewall-cmd --perm --zone=internal --dhcpv6-client
+firewall-cmd --perm --zone=internal --remove-service=samba-client
+firewall-cmd --perm --zone=internal --remove-service=dhcpv6-client
 sleep 10s
 reboot now
